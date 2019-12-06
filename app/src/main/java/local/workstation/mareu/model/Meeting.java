181ec12a5f82fb495d7309162928bfd58add2e48
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2019-12-06 01:28:05 +0100

    Adding meeting model

--- app/src/main/java/local/workstation/mareu/model/Meeting.java
+++ app/src/main/java/local/workstation/mareu/model/Meeting.java
@@ -1,5 +1,42 @@
 package local.workstation.mareu.model;
 
-public class Meeting {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Meeting {
+    private static Integer sLastId = 0;
+
+    private static Integer mId;
+    private String mRoomName;
+    private Date mDatetime;
+    private String mTopic;
+    private List<String> mParticipants;
+
+    /**
+     * Constructor
+     * @param topic topic of the meeting
+     * @param roomName name of the meeting room
+     * @param datetime meeting time
+     */
+    public Meeting(String roomName, Date datetime, String topic) {
+        this(roomName, datetime, topic, new ArrayList<String>());
+    }
+
+    /**
+     * Constructor
+     * @param topic topic of the meeting
+     * @param roomName name of the meeting room
+     * @param datetime meeting time
+     * @param participants list of email addresses of meeting participants
+     */
+    public Meeting(String roomName, Date datetime, String topic, List<String> participants) {
+        sLastId++;
+
+        mId = sLastId;
+        mRoomName = roomName;
+        mDatetime = datetime;
+        mTopic = topic;
+        mParticipants = participants;
+    }
 }
