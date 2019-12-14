ad027e6c84d21a869e8878dace42ccd1e8e3e645
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2019-12-14 14:52:17 +0100

    Tests: fix warning  may produce NullPointerException

--- app/src/androidTest/java/local/workstation/mareu/utils/RecyclerViewItemCountAssertion.java
+++ app/src/androidTest/java/local/workstation/mareu/utils/RecyclerViewItemCountAssertion.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.ViewAssertion;
 
+import java.util.Objects;
+
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -24,6 +26,6 @@ public class RecyclerViewItemCountAssertion implements ViewAssertion {
 
         RecyclerView recyclerView = (RecyclerView) view;
         RecyclerView.Adapter adapter = recyclerView.getAdapter();
-        assertThat(adapter.getItemCount(), is(expectedCount));
+        assertThat(Objects.requireNonNull(adapter).getItemCount(), is(expectedCount));
     }
 }
