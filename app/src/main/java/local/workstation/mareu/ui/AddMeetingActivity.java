9376d6e125163f3d85ac5ad0177d38ab17211799
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2019-12-19 22:08:19 +0100

    Validate text input layout before confirming the addition of a meeting

--- app/src/main/java/local/workstation/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/local/workstation/mareu/ui/AddMeetingActivity.java
@@ -3,14 +3,67 @@ package local.workstation.mareu.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import local.workstation.mareu.R;
 
+/**
+ * Add new meeting
+ */
 public class AddMeetingActivity extends AppCompatActivity {
+
+    private TextInputLayout mRoomNameTextInputLayout;
+    private TextInputLayout mTopicTextInputLayout;
+
+    private Button mCancelButton;
+    private Button mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meeting);
+
+        mRoomNameTextInputLayout = findViewById(R.id.room_name);
+        mTopicTextInputLayout = findViewById(R.id.topic);
+
+        mCancelButton = findViewById(R.id.cancel_button);
+        mAddButton = findViewById(R.id.add_button);
+
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), R.string.abort_add_meeting, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateTextInputLayout(mRoomNameTextInputLayout) | !validateTextInputLayout(mTopicTextInputLayout)) {
+                    Toast.makeText(v.getContext(), R.string.error_add_new_meeting, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(v.getContext(), R.string.add_new_meeting, Toast.LENGTH_LONG).show();
+                // TODO returns serialized data
+                finish();
+            }
+        });
+    }
+
+    private boolean validateTextInputLayout(TextInputLayout inputValue) {
+        String tmpValue = inputValue.getEditText().getText().toString().trim();
+
+        if (tmpValue.isEmpty()) {
+            inputValue.setError(getText(R.string.error_field_empty));
+            return false;
+        } else {
+            inputValue.setError(null);
+            return true;
+        }
     }
 }
