ac15d5efdc931383414998c20e87764824924c7c
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2019-12-19 03:26:09 +0100

    Adding the ability to add a meeting

---
+++ app/src/main/java/local/workstation/mareu/ui/AddMeetingActivity.java
@@ -0,0 +1,16 @@
+package local.workstation.mareu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import local.workstation.mareu.R;
+
+public class AddMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meeting);
+    }
+}
--- app/src/main/java/local/workstation/mareu/ui/meeting_list/ListMeetingActivity.java
+++ app/src/main/java/local/workstation/mareu/ui/meeting_list/ListMeetingActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import local.workstation.mareu.R;
 import local.workstation.mareu.di.DI;
 import local.workstation.mareu.service.MeetingApiService;
+import local.workstation.mareu.ui.AddMeetingActivity;
 
 /**
  * Display list of meetings (main activity)
@@ -16,9 +21,11 @@ import local.workstation.mareu.service.MeetingApiService;
  * Use an fake meeting API Service to manage meetings.
  */
 public class ListMeetingActivity extends AppCompatActivity {
+    private static final int ADD_MEETING_ACTIVITY_REQUEST_CODE = 1;
     private MeetingApiService mApiService;
     private RecyclerView mRecyclerView;
     private ItemMeetingRecyclerViewAdapter mItemMeetingRecyclerViewAdapter;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +36,16 @@ public class ListMeetingActivity extends AppCompatActivity {
 
         mRecyclerView = findViewById(R.id.list);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         mItemMeetingRecyclerViewAdapter = new ItemMeetingRecyclerViewAdapter(this, mApiService);
-
         mRecyclerView.setAdapter(mItemMeetingRecyclerViewAdapter);
+
+        mFloatingActionButton = findViewById(R.id.add);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addMeetingActivity = new Intent(ListMeetingActivity.this, AddMeetingActivity.class);
+                startActivityForResult(addMeetingActivity, ADD_MEETING_ACTIVITY_REQUEST_CODE);
+            }
+        });
     }
 }
